d8522126181a3c6796b8fbcb18c5308a002ba0d0
Johnny Hu <heyitsjhu@gmail.com>
2018-05-21 23:21:18 -0700

    Rename Schema folder to _schema

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { ButtonSchema } from '../../Schema';
+import { ButtonSchema } from '_schema';
 
 class Button extends Component {
   constructor(props) {
