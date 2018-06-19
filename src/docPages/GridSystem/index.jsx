7385f51be95091de0caaf47f34ed5ad359e287b8
Johnny Hu <heyitsjhu@gmail.com>
2018-06-18 22:29:21 -0700

    Create the initial default grid system using css grids

--- src/docPages/GridSystem/index.jsx
+++ src/docPages/GridSystem/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class GridSystemPage extends Component {
   render() {
@@ -6,119 +6,125 @@ export default class GridSystemPage extends Component {
       <div className="container-fluid">
         <h1 className="docs heading">Grid System</h1>
         <p>
-          Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio aged decaffeinated espresso. Americano filter coffee blue mountain, extraction, roast, acerbic filter milk skinny doppio. Mocha pumpkin spice percolator, filter carajillo, black sugar cortado caramelization so instant.
+          Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio aged
+          decaffeinated espresso. Americano filter coffee blue mountain,
+          extraction, roast, acerbic filter milk skinny doppio. Mocha pumpkin
+          spice percolator, filter carajillo, black sugar cortado caramelization
+          so instant.
         </p>
         <section className="docs section" name="rows-and-columns">
-            <header className="docs section__header">
-                <h2 className="docs section__title">
-                    Rows and Columns
-                </h2>
-                <p className="docs section__description">
-                    Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio aged decaffeinated espresso. Americano filter coffee blue mountain, extraction, roast, acerbic filter milk skinny doppio. Mocha pumpkin spice percolator, filter carajillo, black sugar cortado caramelization so instant. Grinder shop, con panna dark galão french press robust froth. Wings percolator, wings, irish seasonal black single shot, sit spoon galão seasonal java.
-                </p>
-            </header>
-            <div className="docs example">
-                <div className="docs card">
-                    <div className="container-fluid">
-                    </div>
-                    <div className="container-fluid">
-                        
-                    </div>
-                    <div className="container-fluid">
-                        
-                    </div>
-                    <div className="container-fluid">
-                        
-                    </div>
-                    <div className="container-fluid">
-                        
-                    </div>
-                    <div className="container-fluid">
-                        
-                    </div>
-                </div>
+          <div className="docs section__header">
+            <h2 className="docs section__title">Rows and Columns</h2>
+            <p className="docs section__description">
+              Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio
+              aged decaffeinated espresso. Americano filter coffee blue
+              mountain, extraction, roast, acerbic filter milk skinny doppio.
+              Mocha pumpkin spice percolator, filter carajillo, black sugar
+              cortado caramelization so instant. Grinder shop, con panna dark
+              galão french press robust froth. Wings percolator, wings, irish
+              seasonal black single shot, sit spoon galão seasonal java.
+            </p>
+          </div>
+          <div className="docs example">
+            <div className="docs card">
+              <div className="container-fluid" />
+              <div className="container-fluid" />
+              <div className="container-fluid" />
+              <div className="container-fluid" />
+              <div className="container-fluid" />
+              <div className="container-fluid" />
             </div>
+          </div>
         </section>
 
         <section className="docs section" name="column-wrapping">
-            <header className="docs section__header">
-                <h2 className="docs section__title">
-                    Column Offset
-                </h2>
-                <p className="docs section__description">
-                    Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio aged decaffeinated espresso. Americano filter coffee blue mountain, extraction, roast, acerbic filter milk skinny doppio.
-                </p>
-            </header>
-            <div className="docs example">
-                <div className="docs card">
-                    <div className="container-fluid">
-                        
-                    </div>
-                </div>
+          <div className="docs section__header">
+            <h2 className="docs section__title">Column Offset</h2>
+            <p className="docs section__description">
+              Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio
+              aged decaffeinated espresso. Americano filter coffee blue
+              mountain, extraction, roast, acerbic filter milk skinny doppio.
+            </p>
+          </div>
+          <div className="docs example">
+            <div className="docs card">
+              <div className="container-fluid" />
             </div>
+          </div>
         </section>
 
         <section className="docs section" name="column-wrapping">
-            <header className="docs section__header">
-                <h2 className="docs section__title">
-                    Column Wrapping
-                </h2>
-                <p className="docs section__description">
-                    Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio aged decaffeinated espresso. Americano filter coffee blue mountain, extraction, roast, acerbic filter milk skinny doppio.
-                </p>
-            </header>
-            <div className="docs example">
-                <div className="docs card">
-                    <div className="container-fluid">
-                        
-                    </div>
-                </div>
+          <div className="docs section__header">
+            <h2 className="docs section__title">Column Wrapping</h2>
+            <p className="docs section__description">
+              Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio
+              aged decaffeinated espresso. Americano filter coffee blue
+              mountain, extraction, roast, acerbic filter milk skinny doppio.
+            </p>
+          </div>
+          <div className="docs example">
+            <div className="docs card">
+              <div className="container-fluid" />
             </div>
+          </div>
         </section>
 
         <section className="docs section" name="nested-columns">
-            <header className="docs section__header">
-                <h2 className="docs section__title">
-                    Nested Columns
-                </h2>
-                <p className="docs section__description">
-                    Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio aged decaffeinated espresso. Americano filter coffee blue mountain, extraction, roast, acerbic filter milk skinny doppio.
-                </p>
-            </header>
-            <div className="docs example">
-                <div className="docs card">
-                    <div className="container-fluid">
-                        <div className="grid">
-                            <div className="grid__col grid__col-sm-9 grid__col-md-4 grid__col-lg-2">
-                                .grid__col .grid__col-sm-9 .grid__col-md-4 .grid__col-lg-2
-                            </div>
-                        </div>
-                        <div className="grid">
-                            <div className="grid__col-3 grid__col-sm-4 grid__col-sm-offset-2">
-                                .grid__col-3 .grid__col-sm-4 .grid__col-sm-offset-2
-                            </div>
-                        </div>
-                        <div className="grid">
-                            <div className="grid__col-9">
-                                .grid__col-9
-                            </div>
-                            <div className="grid__col-3">
-                                .grid__col--3
-                            </div>
-                        </div>
-                        <div className="grid">
-                            <div className="grid__col-4">
-                                .grid__col-4
-                            </div>
-                            <div className="grid__col">
-                                .grid__col
-                            </div>
-                        </div>
-                    </div>
+          <div className="docs section__header">
+            <h2 className="docs section__title">Nested Columns</h2>
+            <p className="docs section__description">
+              Sit, ristretto, body, cinnamon, cup con panna, bar robust doppio
+              aged decaffeinated espresso. Americano filter coffee blue
+              mountain, extraction, roast, acerbic filter milk skinny doppio.
+            </p>
+          </div>
+          <div className="docs example">
+            <div className="docs card">
+              <div className="container-fluid">
+                {/* <div className="grid">
+                  <div className="grid__col grid__col-sm-9 grid__col-md-4 grid__col-lg-2">
+                    .grid__col .grid__col-sm-9 .grid__col-md-4 .grid__col-lg-2
+                  </div>
                 </div>
+                <div className="grid">
+                  <div className="grid__col-3 grid__col-sm-4 grid__col-sm-offset-2">
+                    .grid__col-3 .grid__col-sm-4 .grid__col-sm-offset-2
+                  </div>
+                </div>
+                <div className="grid">
+                  <div className="grid__col-9">.grid__col-9</div>
+                  <div className="grid__col-3">.grid__col--3</div>
+                </div> */}
+                <div className="grid grid--12">
+                  <div className="grid__col">
+                    .grid__col with a lot of text? or enough to go on a second
+                    line
+                  </div>
+                  <div className="grid__col-2">.grid__coldsfsfsdfds</div>
+                  <div className="grid__col-3">.grid__col</div>
+                  <div className="grid__col-start-10 grid__col">.grid__col</div>
+                  <div className="grid__col">.grid__col</div>
+                  <div className="grid__col-start-4 grid__col-2">
+                    .grid__col
+                  </div>
+                  <div className="grid__col-9">.grid__col</div>
+                  <div className="grid__col">.grid__col</div>
+                  <div className="grid__col-6">.grid__col</div>
+                  <div className="grid__col">.grid__col</div>
+                  <div className="grid__col">.grid__col</div>
+                  <div className="grid__col-5">.grid__col</div>
+                  <div className="grid__col-2">.grid__col</div>
+                  <div className="grid__col">.grid__col</div>
+                  <div className="grid__col-start-2 grid__col-6">
+                    .grid__col
+                  </div>
+                  <div className="grid__col-start-12 grid__col">.grid__col</div>
+                </div>
+              </div>
             </div>
+          </div>
         </section>
       </div>
-    )
+    );
   }
 }
--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import BaselinePage from '../docPages/Baseline';
-import GridSystemPage from '../docPages/GridSystem';
-import ButtonsPage from '../docPages/Buttons';
+import BaselinePage from "../docPages/Baseline";
+import GridSystemPage from "../docPages/GridSystem";
+import ButtonsPage from "../docPages/Buttons";
 
 class AppRoutes extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={BaselinePage} />
+        <Route exact path="/" component={GridSystemPage} />
         <Route exact path="/p/baseline" component={BaselinePage} />
         <Route exact path="/p/grid-system" component={GridSystemPage} />
         <Route exact path="/p/buttons" component={ButtonsPage} />
